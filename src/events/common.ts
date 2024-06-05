faa5f0fc00c620df0f01999c816287975e116b62
Alexander Ng <alexng353@gmail.com>
2024-06-04 23:47:18 -0700

    confused

--- src/events/common.ts
+++ src/events/common.ts
@@ -19,6 +19,7 @@ export class Example {
     [message]: ArgsOf<"messageCreate">,
     client: Client,
   ): Promise<void> {
+    console.log("Message Created", client.user?.username, message.content);
     const url_regex = /(https?:\/\/\S+)/g;
     const urls =
       message.content.match(url_regex)?.map((url) => new URL(url)) ?? [];
